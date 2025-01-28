dcf65f6c6e6d32eb3233ffd2bf5e7107826f42af
Fábio Oliveira <fabio20rj@gmail.com>
2025-01-28 18:32:31 +0100

    feat: add cors in backend

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
     "passwords",
 ]
 
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -91,6 +94,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Password validation
